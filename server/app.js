e7ea89d6ee1b01d39a08217877630ca4b8e405c2
wancat <wancat@wancat.cc>
2021-02-24 10:42:56 +0800

    Connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,19 +1,29 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
+import mongoose from 'mongoose'
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+mongoose.connect('mongodb://localhost/life-navigator-dev', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error: '))
+db.on('connected', () => console.log('Database connected'))
 
 export default app
